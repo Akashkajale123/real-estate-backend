0ff7fc444288bdf802b06b547358c598876a2e9c
Akash Kajale <akashkajale125@gmail.com>
2024-03-13 13:08:28 +0530

    .

--- src/Routes/propertyRoutes.js
+++ src/Routes/propertyRoutes.js
@@ -1,34 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const validateUser = require("../MiddleWares/validateUser.js");
 const propertyControllers = require("../Controllers/propertyControllers.js");
-const multer = require("multer"); // Import Multer
-const cloudinary = require("cloudinary").v2;
 
-// Configure Cloudinary
-cloudinary.config({
-	cloud_name:"dfigkhihk",
-	api_key:"546981989443856",
-	api_secret:"6CeEnnAqTxB4Bu1qD_sM-xZMlGE",
-});
 
-// Configure multer
-const upload = multer({ dest: "uploads/" }); // Set upload destination
-
-// Route for handling file uploads
-console.log(cloudinary.uploader)
-router.post("/upload", upload.single("photo"), async (req, res) => {
-	try {
-		// Upload file to Cloudinary
-		const result = await cloudinary.uploader.upload(req.file.path);
-
-		// Return the URL of the uploaded image
-		res.json({ url: result.secure_url });
-	} catch (error) {
-		console.error("Error uploading image:", error);
-		res.status(500).json({ message: "Server Error" });
-	}
-});
 
 router.post("/addproperty/:id", propertyControllers.addProperty);
 router.get("/getAllProperties/:id", propertyControllers.getAllProperties);
