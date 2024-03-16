f86f071a7bd5074c978e6b099676e43b94d251f1
Akash Kajale <119407946+Akashkajale123@users.noreply.github.com>
2024-03-16 23:38:53 +0530

    mongodb password update

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ dotEnv.config();
 const app = express();
 const PORT = process.env.PORT;
 
-const URI ="mongodb+srv://akashkajale125:BuzgMLM7KAsGVGix@cluster1.fnodolx.mongodb.net/Real_Estate_properties?retryWrites=true&w=majority";
+const URI =`mongodb+srv://akashkajale125:${encodeURIComponent("Akash@123")}@cluster1.fnodolx.mongodb.net/Real_Estate_properties?retryWrites=true&w=majority`;
 
 // Database connection
 const connectDB = async () => {
